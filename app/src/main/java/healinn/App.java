254179baf5cbad964012eea179b86001ceb970af
fadhiyahsyafikah <fadhiyah2007@gmail.com>
2026-05-28 21:08:11 +0800

    feat: controller, login select

--- app/src/main/java/healinn/App.java
+++ app/src/main/java/healinn/App.java
@@ -1,9 +1,16 @@
-// package healinn;
+package healinn;
 
+import healinn.controller.*;
+import healinn.service.Database;
+import healinn.service.DatabaseSeeder;
+import healinn.util.SceneManager;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-// public class App extends Application {
+public class App extends Application {
 
-//     public static void main(String[] args) {
-//         launch(args);
-//     }
-// }
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
